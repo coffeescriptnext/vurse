2a0ad98fc0a85790f541debe1750cae782e7c199
Tony Burns <tony@tabolario.com>
2015-01-14 14:14:50 -0500

    Add CHANGELOG.md and VERSION module

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# Change history for Snippety
+
+## [v0.1.0](https://github.com/tabolario/snippety/releases/tag/v0.1.0) (in development)
+
+* Initial release
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,15 @@ require 'sprockets/railtie'
 Bundler.require(*Rails.groups)
 
 module Snippety
+  module VERSION
+    MAJOR = 1
+    MINOR = 0
+    PATCH = 0
+    PRE = 'alpha'
+
+    STRING = [MAJOR, MINOR, PATCH, PRE].compact.join('.')
+  end
+
   class Application < Rails::Application
     # Snippety configuration
     config.snippety = config_for(:snippety).deep_symbolize_keys
